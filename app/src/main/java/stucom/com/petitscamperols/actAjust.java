c81abee0658162bcd9990dd2885b1dd04ee1f561
MrIllas <robert.re.ill1998@gmail.com>
2018-10-30 16:00:49 +0100

    CAMERA

--- app/src/main/java/stucom/com/petitscamperols/actAjust.java
+++ app/src/main/java/stucom/com/petitscamperols/actAjust.java
@@ -1,15 +1,23 @@
 package stucom.com.petitscamperols;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class actAjust extends AppCompatActivity {
 
     EditText userName;
     EditText userEmail;
+    Uri selectedImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,28 @@ public class actAjust extends AppCompatActivity {
 
         userName = findViewById(R.id.userName);
         userEmail = findViewById(R.id.userEmail);
+        Button btnCamera = findViewById(R.id.btnCamera);
+        Button btnGaleria = findViewById(R.id.btnGaleria);
+        final ImageView imageview = findViewById(R.id.imageView);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);
+                selectedImage = takePicture.getData();
+                imageview.setImageURI(selectedImage);
+
+            }
+        });
+        btnGaleria.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+                selectedImage = pickPhoto.getData();
+                imageview.setImageURI(selectedImage);
+            }
+        });
     }
 
     @Override
@@ -28,7 +58,7 @@ public class actAjust extends AppCompatActivity {
         confData.putString("userName", userName.getText().toString());
         confData.putString("userEmail", userEmail.getText().toString());
         confData.commit();
-        Toast.makeText(getApplicationContext(), "Datos guardados", Toast.LENGTH_LONG);
+        Toast.makeText(actAjust.this, "Datos guardados", Toast.LENGTH_LONG);
     }
 
     @Override
@@ -37,6 +67,6 @@ public class actAjust extends AppCompatActivity {
         SharedPreferences mypref = getPreferences(MODE_PRIVATE);
         userName.setText(mypref.getString("userName", ""));
         userEmail.setText(mypref.getString("userEmail", ""));
-
+        //imageview.setImageURI(selectedImage);
     }
 }
